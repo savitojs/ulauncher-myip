ba6001fa9a86ca1d1a52158d012ec79420da9eb2
Savitoj Singh <savsingh@redhat.com>
2024-03-23 21:31:31 -0400

    Fix the clipboard
    Signed-off-by: Savitoj Singh <savsingh@redhat.com>

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import urllib.request, urllib.error, urllib.parse
+import urllib.request
 import logging
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
@@ -11,24 +11,24 @@ logger = logging.getLogger(__name__)
 
 class MyIpExtension(Extension):
 
-	def __init__(self):
-		super(MyIpExtension, self).__init__()
-		self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+    def __init__(self):
+        super(MyIpExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
 
 class KeywordQueryEventListener(EventListener):
 
-	def on_event(self, event, extension):
-		ip = urllib.request.urlopen("http://icanhazip.com/").read()
-		logger.debug('Got external ip: %s', ip)
+    def on_event(self, event, extension):
+        ip = urllib.request.urlopen("http://icanhazip.com/").read().decode('utf-8')
+        logger.debug('Got external ip: %s', ip)
 
-		items = []
-		items.append(ExtensionResultItem(icon='images/icon.png',
-										name='External IP: %s' % ip.decode(),
-										description='Press \'enter\' to copy to clipboard.',
-										on_enter=CopyToClipboardAction(ip)))
+        items = []
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                         name='External IP: %s' % ip,
+                                         description='Press \'enter\' to copy to clipboard.',
+                                         on_enter=CopyToClipboardAction(ip)))
 
-		return RenderResultListAction(items)
+        return RenderResultListAction(items)
 
 if __name__ == '__main__':
-	MyIpExtension().run()
+    MyIpExtension().run()
